e98c4839cef53dc921ecac83c2b11e7b36133f0b
Kenneth <kenjin.p@gmail.com>
2015-03-27 12:25:02 +0100

    first commit, added gulpfile

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bearded-octo-dangerzone
 Front-End Sign-up Implementation Using SMACSS
+
+Goals
+- use GULP to improve speed and concat production files
+- use node.js browser testing to check frontend javascript
+- use SMACSS to improve style maintainability
+- deploy using Digital Ocean DMS and Nginx
---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+//include gulp
+var gulp = require('gulp');
+
+//include plug-ins
+var jshint = require('gulp-jshint'),
+    changed = require('gulp-changed'),
+    imagemin = require('gulp-imagemin'),
+    minifyHTML = require('gulp-minify-html'),
+    concat = require('gulp-concat'),
+    stripDebug = require('gulp-strip-debug'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    autoprefix = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css');
+
+//JS hint task
+gulp.task('jshint', function () {
+  gulp.src('./src/scripts/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+//minify new images
+gulp.task('imagemin', function() {
+  var imgSrc = './src/images/**/*',
+      imgDst = './build/images';
+
+  gulp.src(imgSrc)
+    .pipe(changed(imgDst))
+    .pipe(imagemin())
+    .pipe(gulp.dest(imgDst));
+});
+
+//minify new or changed HTML pages
+gulp.task('htmlpage', function() {
+  var htmlSrc = './src/*.html',
+      htmlDst = './build';
+
+  gulp.src(htmlSrc)
+    .pipe(changed(htmlDst))
+    .pipe(minifyHTML())
+    .pipe(gulp.dest(htmlDst));
+});
+
+//JS concat, strip debugging then minify
+gulp.task('scripts', function() {
+  gulp.src(['./src/scripts/*.js'])
+    .pipe(concat('script.js'))
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts'));
+});
+
+//CSS sass, concat, auto-prefix and minify
+gulp.task('styles', function() {
+  gulp.src(['./src/styles/*scss'])
+    .pipe(sass())
+    .pipe(concat('syles.css'))
+    .pipe(autoprefix('last 2 versions'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./build/styles/'));
+});
+
+//default gulp task
+gulp.task('default', ['imagemin', 'htmlpage', 'scripts', 'styles'], function() {
+  //watch for HTMl changes
+  gulp.watch('./src/*html', ['htmlpage']);
+  //watch for JS changes
+  gulp.watch('./src/scripts/*.js' ['jshint', 'scripts']);
+  //watch for CSS changes
+  gulp.watch('./src/styles/*.css', ['styles']);
+});
