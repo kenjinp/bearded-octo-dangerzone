5fc8112b0eef64a03a5be63f37581af82bdeb7b2
Kenneth <kenjin.p@gmail.com>
2015-04-05 18:44:42 +0200

    added test runner

---
+++ src/scripts/spec/integration/signup_spec.js
@@ -0,0 +1,123 @@
+'use strict';
+
+//dependencies
+var path = require('path'),
+    Nightmare = require('nightmare'),
+    should = require('chai').should();
+
+//quick fake user
+var bob = {
+  vorname: 'bob',
+  nochname: 'schmidt',
+  mitgliedsname: 'bobschmidt2001',
+  email: 'bob@lol.com',
+  passwort: 'bobisthebest'
+};
+
+describe('Signup Form', function() {
+
+  this.timeout(20000);
+  var url = 'file://' + path.resolve(__dirname, '../../../../build/index.html');
+
+  describe('Is page there?', function() {
+    it('should show form when loaded', function(done) {
+      new Nightmare({webSecurity:false})
+        .goto(url)
+        .evaluate(function() {
+          return document.querySelectorAll('form').length;
+        }, function(result) {
+          result.should.equal(1);
+          done();
+        })
+        .run();
+    });
+  });
+
+  describe('#showPass()', function() {
+    it('should add show-password to end of password input', function(done) {
+      new Nightmare()
+        .goto(url)
+        .evaluate(function() {
+          return document.querySelectorAll('input[name="show-pass"]').length;
+        }, function(result) {
+          result.should.equal(1);
+          done();
+        })
+        .run();
+    });
+  });
+
+  describe('type name into name input', function() {
+    it('it should have the name value in input', function(done) {
+      new Nightmare()
+        .goto(url)
+        .type('input[name="vorname"]',bob.vorname)
+        .evaluate(function() {
+          return document.querySelector('input[name="vorname"]').value;
+        }, function(result) {
+          result.should.equal(bob.vorname);
+          done();
+        })
+        .run();
+    });
+  });
+
+  describe('submit valid form', function() {
+    it('it should show a success message', function(done) {
+      new Nightmare()
+        .goto(url)
+        .type('input[name="vorname"]', bob.vorname)
+        .type('input[name="nachname"]', bob.nachname)
+        .type('input[name="mitgliedsname"]', bob.mitgliedsname)
+        .type('input[name="email"]', bob.email)
+        .type('input[name="passwort"]', bob.passwort)
+        .click('inpit[name="terms"]')
+        .click('.call-to-action')
+        .evaluate(function() {
+            var el = document.querySelector('.success');
+            return el.classList.contains('hidden');
+        }, function(result) {
+          result.should.not.equal(true);
+          done();
+        })
+        .run();
+    });
+  });
+
+  describe('#passwordStrength', function() {
+
+    describe('write crappy password', function() {
+      it('it should indicate bad password', function(done) {
+        new Nightmare()
+          .goto(url)
+          .type('input[name="passwort"]', 'aaaaa')
+          .evaluate(function() {
+              var el = document.querySelector('.pass-strength');
+              return el.classList.contains('warning');
+          }, function(result) {
+            result.should.not.equal(false);
+            done();
+          })
+          .run();
+      });
+    });
+
+    describe('write great password', function() {
+      it('it should indicate good password', function(done) {
+        new Nightmare()
+          .goto(url)
+          .type('input[name="passwort"]', 'aBanana123!@Jupiter')
+          .evaluate(function() {
+              var el = document.querySelector('.pass-strength');
+              return el.classList.contains('success');
+          }, function(result) {
+            result.should.not.equal(false);
+            done();
+          })
+          .run();
+      });
+    });
+
+  });//passwordStrength end
+
+});//SignupFormEnd
--- src/scripts/spec/test.js
+++ src/scripts/spec/test.js
@@ -13,129 +13,35 @@ function getDirectories(srcpath) {
   });
 }
 
-console.log(__dirname);
-console.log(getDirectories(__dirname));
+function getFiles(dir, files_) {
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files) {
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()) {
+            getFiles(name, files_);
+        } else {
+            files_.push(name);
+        }
+    }
+    return files_;
+}
 
-//tests
-var passStrengthSpec = require('./modules/passwordStrength_spec'),
-    integrationSpec = require('./integration/signup_spec')
-
-//run pass strength module;
-passStrengthSpec;
-
-//quick fake user
-var bob = {
-  vorname: 'bob',
-  nochname: 'schmidt',
-  mitgliedsname: 'bobschmidt2001',
-  email: 'bob@lol.com',
-  passwort: 'bobisthebest'
-};
-
-describe('Signup Form', function() {
-
-  this.timeout(20000);
-  var url = 'file://' + path.resolve(__dirname, '../../../build/index.html');
-
-  describe('Is page there?', function() {
-    it('should show form when loaded', function(done) {
-      new Nightmare({webSecurity:false})
-        .goto(url)
-        .evaluate(function() {
-          return document.querySelectorAll('form').length;
-        }, function(result) {
-          result.should.equal(1);
-          done();
-        })
-        .run();
+function runAllTests() {
+  var testTypes = getDirectories(__dirname);
+  for (var i in testTypes) {
+      var testType = testTypes[i];
+      describe(testType, function() {
+      //find all tests in each test type
+      var tests = getFiles(__dirname + '/' + testType);
+      for (var i in tests) {
+        var test = tests[i];
+        var testPath = './' + path.relative(__dirname, test);
+        var runTest = require(testPath);
+        runTest;
+      }
     });
-  });
+  }
+}
 
-  describe('#showPass()', function() {
-    it('should add show-password to end of password input', function(done) {
-      new Nightmare()
-        .goto(url)
-        .evaluate(function() {
-          return document.querySelectorAll('input[name="show-pass"]').length;
-        }, function(result) {
-          result.should.equal(1);
-          done();
-        })
-        .run();
-    });
-  });
-
-  describe('type name into name input', function() {
-    it('it should have the name value in input', function(done) {
-      new Nightmare()
-        .goto(url)
-        .type('input[name="vorname"]',bob.vorname)
-        .evaluate(function() {
-          return document.querySelector('input[name="vorname"]').value;
-        }, function(result) {
-          result.should.equal(bob.vorname);
-          done();
-        })
-        .run();
-    });
-  });
-
-  describe('submit valid form', function() {
-    it('it should show a success message', function(done) {
-      new Nightmare()
-        .goto(url)
-        .type('input[name="vorname"]', bob.vorname)
-        .type('input[name="nachname"]', bob.nachname)
-        .type('input[name="mitgliedsname"]', bob.mitgliedsname)
-        .type('input[name="email"]', bob.email)
-        .type('input[name="passwort"]', bob.passwort)
-        .click('inpit[name="terms"]')
-        .click('.call-to-action')
-        .evaluate(function() {
-            var el = document.querySelector('#success');
-            return el.classList.contains('hidden');
-        }, function(result) {
-          result.should.not.equal(true);
-          done();
-        })
-        .run();
-    });
-  });
-
-  describe('#passwordStrength', function() {
-
-    describe('write crappy password', function() {
-      it('it should indicate bad password', function(done) {
-        new Nightmare()
-          .goto(url)
-          .type('input[name="passwort"]', 'aaaaa')
-          .evaluate(function() {
-              var el = document.querySelector('.pass-strength');
-              return el.classList.contains('warning');
-          }, function(result) {
-            result.should.not.equal(false);
-            done();
-          })
-          .run();
-      });
-    });
-
-    describe('write great password', function() {
-      it('it should indicate good password', function(done) {
-        new Nightmare()
-          .goto(url)
-          .type('input[name="passwort"]', 'aBanana123!@Jupiter')
-          .evaluate(function() {
-              var el = document.querySelector('.pass-strength');
-              return el.classList.contains('success');
-          }, function(result) {
-            result.should.not.equal(false);
-            done();
-          })
-          .run();
-      });
-    });
-
-  });//passwordStrength end
-
-});//SignupFormEnd
+runAllTests();
