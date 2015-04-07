6c674f3e7e7bd7e6d2199de60890a13450a99c85
Kenneth <kenjin.p@gmail.com>
2015-04-07 17:32:27 +0200

    teasing out integration and unit tests for jquery validator\n\n theres lots to be done here but I need to figure out how to write unit tests with a jquery plugin so there is little or no dom manipulation and no reliance on a headless browser

--- src/scripts/spec/integration/modules/jquery.validator_integration.js
+++ src/scripts/spec/integration/modules/jquery.validator_integration.js
@@ -13,6 +13,17 @@ var bob = {
 
 var jqvIntegration = function(url) {
   describe('#jQuery Validator', function() {
+    it('$ should have the jqv function', function(done) {
+      new Nightmare()
+        .goto(url)
+        .evaluate(function() {
+          return $.fn.validator();
+        }, function(result) {
+          result.should.not.equal(null);
+          done();
+        })
+        .run();
+    });
     it('should confirm initialization by adding class "validating" to form',
       function(done) {
         new Nightmare()
@@ -26,44 +37,44 @@ var jqvIntegration = function(url) {
           })
           .run();
       });
-      it('it should show a success message if form submitted and inputs are validated', function(done) {
-        new Nightmare()
-          .goto(url)
-          .type('input[name="vorname"]', bob.vorname)
-          .type('input[name="nachname"]', bob.nachname)
-          .type('input[name="mitgliedsname"]', bob.mitgliedsname)
-          .type('input[name="email"]', bob.email)
-          .type('input[name="passwort"]', bob.passwort)
-          .click('input[name="terms"]')
-          .click('.call-to-action')
-          .evaluate(function() {
-              var el = document.querySelector('.success');
-              return el.classList.contains('hidden');
-          }, function(result) {
-            result.should.not.equal(false);
-            done();
-          })
-          .run();
-      });
-      it('it should show a warning message if form submitted but notvalid', function(done) {
-        new Nightmare()
-          .goto(url)
-          .type('input[name="vorname"]', bob.vorname)
-          .type('input[name="nachname"]', bob.nachname)
-          .type('input[name="mitgliedsname"]', bob.mitgliedsname)
-          .type('input[name="email"]', bob.email)
+    it('it should show a success message if form submitted and inputs are validated', function(done) {
+      new Nightmare()
+        .goto(url)
+        .type('input[name="vorname"]', bob.vorname)
+        .type('input[name="nachname"]', bob.nachname)
+        .type('input[name="mitgliedsname"]', bob.mitgliedsname)
+        .type('input[name="email"]', bob.email)
+        .type('input[name="passwort"]', bob.passwort)
+        .click('input[name="terms"]')
+        .click('.call-to-action')
+        .evaluate(function() {
+          var el = document.querySelector('.success');
+          return el.classList.contains('hidden');
+        }, function(result) {
+          result.should.not.equal(false);
+          done();
+        })
+        .run();
+    });
+    it('it should show a warning message if form submitted but notvalid', function(done) {
+      new Nightmare()
+        .goto(url)
+        .type('input[name="vorname"]', bob.vorname)
+        .type('input[name="nachname"]', bob.nachname)
+        .type('input[name="mitgliedsname"]', bob.mitgliedsname)
+        .type('input[name="email"]', bob.email)
           //.type('input[name="passwort"]', bob.passwort)
-          .click('input[name="terms"]')
-          .click('.call-to-action')
-          .evaluate(function() {
-              var el = document.querySelector('.warning');
-              return el.classList.contains('hidden');
-          }, function(result) {
-            result.should.not.equal(false);
-            done();
-          })
-          .run();
-      });
+        .click('input[name="terms"]')
+        .click('.call-to-action')
+        .evaluate(function() {
+          var el = document.querySelector('.messages.warning');
+          return el.classList.contains('shown');
+        }, function(result) {
+          result.should.equal(true);
+          done();
+        })
+        .run();
+    });
   });
 };
 
